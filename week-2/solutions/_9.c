e22348486a87b18676273a94cf0aad883fcd9821
Lassie <lassie@m.lassie.cf>
2024-10-16 01:43:08 +0300

    fix division by zero

--- week-2/solutions/_9.c
+++ week-2/solutions/_9.c
@@ -4,6 +4,9 @@ int main(void) {
     int a, b;
     scanf("%d %d", &a, &b);
 
-    printf("%d %d\n", a / b, a % b);
+	int division_by_zero = b == 0;
+	division_by_zero && printf("division by zero\n"); // OK, `printf` returns int
+    division_by_zero || printf("quotient: %d\nremainder: %d\n", a / b, a % b);
     return 0;
 }
+// bonus question: apart from division by zero, what other (valid) values of `a` and `b` will cause undefined behavior?
