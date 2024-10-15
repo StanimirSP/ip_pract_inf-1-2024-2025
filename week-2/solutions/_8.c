d8a0bbb9076b9d9072c2f1b5eaa520b6c5f5086b
Lassie <lassie@m.lassie.cf>
2024-10-16 01:12:39 +0300

    refactor

--- week-2/solutions/_8.c
+++ week-2/solutions/_8.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-    int a ='0',
+    int a = '0',
         b = '1',
         c = '9',
         d = 'B',
         e = 'F';
 
-    printf("0 = %d, 1 = %d, 9 = %d, B = %d, F = %d\n", a, b, c, d, e);
-    // printf("0 = %d, 1 = %d, 9 = %d, B = %d, F = %d\n", '0', '1', '9', 'B', 'F');
+    printf("'%c' = %d, '%c' = %d, '%c' = %d, '%c' = %d, '%c' = %d\n",
+		(char)a, a, (char)b, b, (char)c, c, (char)d, d, (char)e, e);
+    // printf("'0' = %d, '1' = %d, '9' = %d, 'B' = %d, 'F' = %d\n", '0', '1', '9', 'B', 'F');
     return 0;
 }
