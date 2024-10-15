87c75318dcbf2835b3f6d93bf4b54a6a46a1e02c
Lassie <lassie@m.lassie.cf>
2024-10-16 01:58:59 +0300

    simplify

--- week-2/solutions/_2.c
+++ week-2/solutions/_2.c
@@ -5,8 +5,9 @@ int main(void){
     const double pi = 3.1415926536; // one may also decide to use e.g. acos(-1)
     scanf("%lf", &r);
     int isValid = r > 0;
+	isValid || printf("Invalid input\n");
 	const double circumference = 2 * pi * r;
     const double area = r * r * pi;
-    ( isValid && printf("Circumference: %.2lf\nArea: %.2lf\n", circumference, area) ) || printf("Invalid data!\n");
+    isValid && printf("Circumference: %.2lf\nArea: %.2lf\n", circumference, area);
     return 0;
 }
