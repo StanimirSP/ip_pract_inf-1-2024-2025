20cb089fddb52e25696cd33a3a9223dfbca9276f
Vasil Petkov <vasilepetkov@gmail.com>
2024-11-28 23:12:16 +0200

    Solutions for problems 6 to 11

---
+++ week-9/solutions/task10.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#define MAX_SIZE 1024
+
+int readIntArray(int arr[], unsigned size) {
+    for (unsigned i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    return size;
+}
+
+void printIntArray(const int arr[], unsigned size) {
+    for (unsigned i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    puts("");
+}
+
+int isPrime(int n) {
+    n = n < 0 ? -n : n;
+    for (unsigned i = 2; i*i <= n; i++) {
+        if(n % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+unsigned removeNumbersDivisibleBy5(int arr[], unsigned size) {
+    for (unsigned i = 0; i < size; i++) {
+        if(arr[i] % 5 ==0) {
+            for (unsigned j = i; j < size - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            size--;
+            // If we don't "i--;" we will go the the next element and skip it
+            // {5, 25, 200, 56}
+            // i = 0 we remove and then arr[i] becomes 25 and we skip it. 
+            // This is why we "i--;" - on the next iteration we stay on the next element that is now the current one.
+            i--;
+        }
+    }
+    return size;
+}
+
+unsigned removeNumbersLessThan100(int arr[], unsigned size) {
+    for (unsigned i = 0; i < size; i++) {
+        if(arr[i] < 100) {
+            for (unsigned j = i; j < size - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            size--;
+            i--;
+        }
+    }
+    return size;
+}
+
+unsigned removePrimeNumbers(int arr[], unsigned size) {
+    for (unsigned i = 0; i < size; i++) {
+        if(isPrime(arr[i])) {
+            for (unsigned j = i; j < size - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            size--;
+            i--;
+        }
+    }
+    return size;
+}
+
+int main() {
+    int intArray[MAX_SIZE];
+    unsigned size;
+    
+    scanf("%u", &size);
+    if(size > MAX_SIZE) {
+        puts("Size should be less than 1025");
+        return 1;
+    }
+    
+    readIntArray(intArray, size);
+
+    size = removeNumbersDivisibleBy5(intArray, size);
+    printIntArray(intArray, size);
+    // or
+    // size = removeNumbersLessThan100(intArray, size);
+    // printIntArray(intArray, size);
+    // or
+    // size = removePrimeNumbers(intArray, size);
+    // printIntArray(intArray, size);
+
+    return 0;
+}
---
+++ week-9/solutions/task9.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#define MAX_SIZE 1024
+
+int readIntArray(int arr[], unsigned size) {
+    for (unsigned i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    return size;
+}
+
+void printIntArray(const int arr[], unsigned size) {
+    for (unsigned i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    puts("");
+}
+
+int isPrime(int n) {
+    n = n < 0 ? -n : n;
+    for (unsigned i = 2; i*i <= n; i++) {
+        if(n % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+unsigned countPositiveNumbersInArray(const int arr[] , unsigned size) {
+    unsigned count = 0;
+    for (unsigned i = 0; i < size; i++) {
+        if(arr[i] > 0)
+            count++;
+    }
+    return count;
+}
+
+unsigned countEvenNumbersInArray(const int arr[] , unsigned size) {
+    unsigned count = 0;
+    for (unsigned i = 0; i < size; i++) {
+        if(arr[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+unsigned countPrimeNumbersInArray(const int arr[] , unsigned size) {
+    unsigned count = 0;
+    for (unsigned i = 0; i < size; i++) {
+        if(isPrime(arr[i]))
+            count++;
+    }
+    return count;
+}
+
+int main() {
+    int intArray[MAX_SIZE];
+    unsigned size;
+    
+    scanf("%u", &size);
+    if(size > MAX_SIZE) {
+        puts("Size should be less than 1025");
+        return 1;
+    }
+    
+    readIntArray(intArray, size);
+
+    unsigned countPositive = countPositiveNumbersInArray(intArray, size);
+    printf("There are %u positive numbers in this array.", countPositive);
+    
+    unsigned countEven = countEvenNumbersInArray(intArray, size);
+    printf("There are %u even numbers in this array.", countEven);
+
+    unsigned countPrime = countPrimeNumbersInArray(intArray, size);
+    printf("There are %u prime numbers in this array.", countPrime);
+
+    return 0;
+}
